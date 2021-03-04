818706b923024f892af75afef466e1c8a911ceba
Tehpson <jesper@familjen-pettersson.se>
2021-03-04 20:22:21 +0100

    added draw table

---
+++ BlackJack_TDD/Main/DrawTable.cs
@@ -0,0 +1,18 @@
+﻿namespace BlackJack_TDD.Main
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class DrawTable
+    {
+        public static void DrawGameTabel()
+        {
+
+        }
+        public static void DrawScoreTable()
+        {
+
+        }
+    }
+}
